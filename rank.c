99b8cb52b8332549ba17a7cbd40e0f686c82998a
최승혜 <mutanxx@naver.com>
2019-05-20 15:25:33 +0900

    랭킹 함수 친구입니다  추후에 타자연습 함수 받으면 수정할게요

---
+++ rank.c
@@ -0,0 +1,105 @@
+#include "rank.h"
+/* 랭킹 
+큰 틀 입니다!!함수 받으면 다 수정할게용*/
+/*
+typedef struct rank *rank_ptr;
+typedef struct rank
+{
+	char name[50];
+	int score;
+	rk_ptr link;
+
+};
+rank_ptr tp_rank = NULL;
+rank_ptr game_rank = NULL;
+rank_ptr head;
+rank_ptr tail;
+
+이렇게 연결 리스트를 이용하여 랭킹 관리 예정.
+*/
+
+void sort()
+{
+
+}//연결리스트 점수대로 정렬
+void TP_rank()
+{
+	while (1)
+	{
+		system("cls");
+		printf("TP_rank 함수 호출\n");
+		if (_getch() == 27)
+		{
+			system("cls");
+			break;
+		}
+		else
+		{
+			system("Cls");
+			continue;
+
+		}
+		
+	}
+	return;
+
+
+	
+
+	
+}
+void CP_rank()
+{
+
+
+	while (1)
+	{
+		system("cls");
+		printf("CP_rank 함수 호출\n");
+		if (_getch() == 27)
+		{
+			system("cls");
+			break;
+		}
+		else
+		{
+			system("Cls");
+			continue;
+
+		}
+	}
+	return;
+
+
+}
+void rank_print()
+{
+	/*
+	printf("┌──────────────────────────────────────────────────────────────────────────────────────────┐\n");
+	printf("│             타자 연습 랭킹                                  게임 랭킹                      │\n");
+	printf("│       이름                   점수               이름                     점수             │\n");
+	printf("│     1.%s                     %d           1.    %s                      %d              │\n",);//예시
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("│                                                                                         │\n");
+	printf("└─────────────────────────────────────────────────────────────────────────────────────────┘\n");
+	임시쓰 */
+
+}
---
+++ rank.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void TP_rank();
+void CP_rank();
+void rank_print();
+void sort();
