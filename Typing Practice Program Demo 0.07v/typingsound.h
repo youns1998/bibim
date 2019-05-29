1a16a3743b03a4c08296e14cf1bdf37f30e09df4
최승혜 <mutanxx@naver.com>
2019-05-30 00:06:30 +0900

    무려 ☆버그수정판★

---
+++ Typing Practice Program Demo 0.07v/typingsound.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<stdio.h>
+#include<Windows.h>
+#include<mmsystem.h>
+#pragma comment(lib,"Winmm.lib")
+
+
+void typingsound();
+void titlesound();
+void pokemonsound();
+void tetsound();
+
